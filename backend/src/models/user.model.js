0f405ea5ccd3b30b299b274958c88cb91e279878
Atharva Ghongade <atharvaghongade22@gmail.com>
2025-11-01 12:18:22 +0530

    minor changes

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -15,6 +15,11 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    userPlan: {
+      type: String,
+      enum: ["free", "pro", "enterprise"],
+      default: "free",
+    },
     apiKeys: [
       {
         type: mongoose.Schema.Types.ObjectId,
